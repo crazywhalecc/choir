5c71f79cdada9fbfd69c16702571387c056c317e
crazywhalecc <jesse2061@outlook.com>
2022-11-18 23:42:48 +0800

    add example

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Choir
 
 一个纯 PHP 写的 Server 库，由 Workerman 改进而来，作为一个学习的项目而编写。
+
+## HTTP 服务器样例
+
+```php
+require_once 'vendor/autoload.php';
+
+// 也可以使用 Url 模式：$server = new \Choir\Server("http://0.0.0.0:20001");
+$server = new \Choir\Http\Server('0.0.0.0', 20001, false, [
+    'worker-num' => 8,
+]);
+
+$server->on('request', function (Choir\Protocol\HttpConnection $connection) {
+    $connection->end('hello world');
+});
+
+$server->start();
+```
--- choir-test.php
+++ choir-test.php
@@ -8,19 +8,7 @@ require_once 'vendor/autoload.php';
 
 $server = new \Choir\Http\Server('0.0.0.0', 20001, false, [
     'worker-num' => 8,
-    // 'logger-level' => 'debug',
 ]);
-
-$server->on('workerstart', function () {
-    // xhprof_enable();
-});
-
-$server->on('workerstop', function () {
-    // $data = xhprof_disable();
-    // $x = new XHProfRuns_Default();
-    // $id = $x->save_run($data, 'xhprof_testing');
-    // echo "http://127.0.0.1:8080/index.php?run={$id}&source=xhprof_testing\n";
-});
 
 $server->on('request', function (Choir\Protocol\HttpConnection $connection) {
     $connection->end('hello world');
